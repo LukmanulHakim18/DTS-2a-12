923d48dd54362a6a6d25aecde1db16f9b5a9e52e
Lukmanul Hakim <lukman.virg@gmail.com>
2021-06-21 01:33:28 +0700

    final comit

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ urlApi = {
     "globalPositif": "https://api.kawalcorona.com/positif",
     "globalSembuh": "https://api.kawalcorona.com/sembuh",
     "globalMeninggal": "https://api.kawalcorona.com/meninggal",
+    "provinsi": "https://api.kawalcorona.com/indonesia/provinsi",
 }
 
 
@@ -21,7 +22,7 @@ def dataIndonesia():
 def dataGlobalPositif():
     data = requests.get(urlApi['globalPositif']).json()
     number = re.sub('[^0-9]', '', data['value'])
-    return int(number)
+    return number
 
 
 def dataGlobalSembuh():
@@ -37,13 +38,87 @@ def dataGlobalMeninggal():
     return int(number)
 
 
+def dataProvinsiRiau(dataIndonesia):
+    dataIndonesia = dataIndonesia
+    data = requests.get(urlApi['provinsi']).json()
+    newData = data[8]['attributes']
+    newData['persentasiPositif'] = newData['Kasus_Posi'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['positif']))
+    newData['persentasiSembuh'] = newData['Kasus_Semb'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['sembuh']))
+    newData['persentasiMeninggal'] = newData['Kasus_Meni'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['meninggal']))
+    newData['persentasiPositif'] = round(newData['persentasiPositif'], 4)
+    newData['persentasiSembuh'] = round(newData['persentasiSembuh'], 4)
+    newData['persentasiMeninggal'] = round(newData['persentasiMeninggal'], 4)
+
+    return newData
+
+
+def dataProvinsiNTB(dataIndonesia):
+    dataIndonesia = dataIndonesia
+    data = requests.get(urlApi['provinsi']).json()
+    newData = data[24]['attributes']
+    newData['persentasiPositif'] = newData['Kasus_Posi'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['positif']))
+    newData['persentasiSembuh'] = newData['Kasus_Semb'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['sembuh']))
+    newData['persentasiMeninggal'] = newData['Kasus_Meni'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['meninggal']))
+    newData['persentasiPositif'] = round(newData['persentasiPositif'], 4)
+    newData['persentasiSembuh'] = round(newData['persentasiSembuh'], 4)
+    newData['persentasiMeninggal'] = round(newData['persentasiMeninggal'], 4)
+
+    return newData
+
+
+def dataProvinsiSulut(dataIndonesia):
+    dataIndonesia = dataIndonesia
+    data = requests.get(urlApi['provinsi']).json()
+    newData = data[15]['attributes']
+    newData['persentasiPositif'] = newData['Kasus_Posi'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['positif']))
+    newData['persentasiSembuh'] = newData['Kasus_Semb'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['sembuh']))
+    newData['persentasiMeninggal'] = newData['Kasus_Meni'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['meninggal']))
+    newData['persentasiPositif'] = round(newData['persentasiPositif'], 4)
+    newData['persentasiSembuh'] = round(newData['persentasiSembuh'], 4)
+    newData['persentasiMeninggal'] = round(newData['persentasiMeninggal'], 4)
+
+    return newData
+
+
+def dataProvinsiJateng(dataIndonesia):
+    dataIndonesia = dataIndonesia
+    data = requests.get(urlApi['provinsi']).json()
+    newData = data[2]['attributes']
+    newData['persentasiPositif'] = newData['Kasus_Posi'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['positif']))
+    newData['persentasiSembuh'] = newData['Kasus_Semb'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['sembuh']))
+    newData['persentasiMeninggal'] = newData['Kasus_Meni'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['meninggal']))
+    newData['persentasiPositif'] = round(newData['persentasiPositif'], 4)
+    newData['persentasiSembuh'] = round(newData['persentasiSembuh'], 4)
+    newData['persentasiMeninggal'] = round(newData['persentasiMeninggal'], 4)
+
+    return newData
+
+
 @app.route("/")
 def index():
     return render_template("index.html",
                            dataIndonesia=dataIndonesia(),
                            dataGlobalPositif=dataGlobalPositif(),
                            dataGlobalSembuh=dataGlobalSembuh(),
-                           dataGlobalMeninggal=dataGlobalMeninggal()
+                           dataGlobalMeninggal=dataGlobalMeninggal(),
+                           dataProvinsiRiau=dataProvinsiRiau(dataIndonesia()),
+                           dataProvinsiSulut=dataProvinsiSulut(
+                               dataIndonesia()),
+                           dataProvinsiJateng=dataProvinsiJateng(
+                               dataIndonesia()),
+                           dataProvinsiNTB=dataProvinsiNTB(dataIndonesia())
                            )
 
 
--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import requests
 import re
 
+
 urlApi = {
     "indonesia": "https://api.kawalcorona.com/indonesia",
     "globalPositif": "https://api.kawalcorona.com/positif",
     "globalSembuh": "https://api.kawalcorona.com/sembuh",
     "globalMeninggal": "https://api.kawalcorona.com/meninggal",
+    "provinsi": "https://api.kawalcorona.com/indonesia/provinsi",
 }
 
 
@@ -14,10 +16,36 @@ def dataIndonesia():
     return data[0]
 
 
+def dataGlobalPositif():
+    data = requests.get(urlApi['globalPositif']).json()
+    number = re.sub('[^0-9]', '', data['value'])
+    return number
+
+
 def dataGlobalSembuh():
+
     data = requests.get(urlApi['globalSembuh']).json()
     number = re.sub('[^0-9]', '', data['value'])
     return int(number)
 
 
-print(dataGlobalSembuh())
+def dataGlobalMeninggal():
+    data = requests.get(urlApi['globalMeninggal']).json()
+    number = re.sub('[^0-9]', '', data['value'])
+    return int(number)
+
+
+def dataProvinsiRiau(dataIndonesia):
+    dataIndonesia = dataIndonesia
+    data = requests.get(urlApi['provinsi']).json()
+    newData = data[8]['attributes']
+    newData['persentasiPositif'] = newData['Kasus_Posi'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['positif']))
+    newData['persentasiSembuh'] = newData['Kasus_Semb'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['sembuh']))
+    newData['persentasiMeninggal'] = newData['Kasus_Meni'] / \
+        int(re.sub('[^0-9]', '', dataIndonesia['meninggal']))
+    return newData
+
+
+print(dataProvinsiRiau(dataIndonesia()))
